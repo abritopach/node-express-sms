34965886fb4ef005bf586d2d1a336b14b364b7e8
Adrián Brito Pacheco <adrbrpa1988@gmail.com>
2018-09-11 16:30:06 +0200

    Added nexmo code

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,17 @@ router.post('/', (req, res) => {
   let text = req.body.text;
 
   // Sending SMS via Nexmo.
+  nexmo.message.sendSms(
+    NUMBER, number, text, {type: 'unicode'},
+    (err, responseData) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.dir(responseData);
+        // Optional: add socket.io -- will explain later
+      }
+    }
+  );
 });
 
 module.exports = router;
