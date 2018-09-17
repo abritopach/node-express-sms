e5bf078a6ddbbf2876c6b0f77ababb2a53c0d05e
Adrián Brito Pacheco <adrbrpa1988@gmail.com>
2018-09-17 12:47:16 +0200

    Added twilio

--- README.md
+++ README.md
@@ -1,14 +1,22 @@
 # NodeExpressSMS
 
-Sample project that shows how to use Node & Nexmo to send SMS from the browser.
+Sample project that shows how to use Node & (Nexmo | Twilio) to send SMS from the browser.
 
 ## Using the Nexmo SMS API
 
 Sign up for a [Nexmo account](https://dashboard.nexmo.com/sign-up)
 
+## Using the Twilio SMS API
+
+Sign up for a [Twilio account](https://www.twilio.com/try-twilio)
+
 ## Set Nexmo credentials
 
-In routes/index.js change apiKey & apiSecret with your Nexmo credentials.
+Set Environment Variables: NEXMO_API_KEY & NEXMO_API_SECRET
+
+## Set Twilio credentials
+
+Set Environment Variables: TWILIO_ACCOUNT_SID & TWILIO_AUTH_TOKEN
 
 ## Install dependencies and run
 
--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,14 @@ var router = express.Router();
 
 const Nexmo = require('nexmo');
 const nexmo = new Nexmo({
-  apiKey: 'API_KEY',
-  apiSecret: 'API_SECRET',
+  apiKey: process.env.NEXMO_API_KEY,
+  apiSecret: process.env.NEXMO_API_SECRET,
 }, {debug: true});
+
+const twilio = require('twilio') (
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
+);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -18,20 +23,31 @@ router.post('/', (req, res) => {
   console.log(req.body);
   let number = req.body.number;
   let text = req.body.text;
+  let platform = req.body.platform;
 
-  // Sending SMS via Nexmo.
-  nexmo.message.sendSms(
-    "NEXMO", number, text, {type: 'unicode'},
-    (err, responseData) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.dir(responseData);
-        res.send(responseData);
-        // Optional: add socket.io -- will explain later
+  if (platform === 'nexmo') {
+     // Sending SMS via Nexmo.
+    nexmo.message.sendSms(
+      "NEXMO", number, text, {type: 'unicode'},
+      (err, responseData) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.dir(responseData);
+          res.send(responseData);
+          // Optional: add socket.io -- will explain later
+        }
       }
-    }
-  );
+    );
+  }
+  else {
+      twilio.messages.create({
+    from: number,
+    to: number,
+    body: text
+  }).then((messsage) => console.log(message.sid));
+  } 
+
 });
 
 module.exports = router;
